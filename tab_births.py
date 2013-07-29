4ab496a8d37c6e5dd552a70a8f9b14edab092334
jamesellis <jellis1080@gmail.com>
2013-07-29 13:36:42 -0700

    Subfunction 'getBirths()' will generate born-on times, origins, method of birth for all tabs in a data slice from hadoop

--- tab_births.py
+++ tab_births.py
@@ -1,6 +1,8 @@
-
 import re
 import json
+
+tabIDs = json.load(open("Tabs_for_127raw.json"))
+lines=open("raw-127.txt").readlines()
 
 def findMoves(tabID,index,movedTabs,lines):
 	if not(tabID in movedTabs):
@@ -14,104 +16,143 @@ def findMoves(tabID,index,movedTabs,lines):
 					break
 	return movedTabs
 
+# def treeTabs():
+
+# ...by context-menu open link in new tab click
+# moneyGrabber(j,'context-openlinkintab',lines,cntxt,this_tab,flag)
+def moneyGrabber(start,searchterm,lines,count,this_tab,flag):
+	for g in range(start-1,start-5,-1):
+		if re.search(searchterm,lines[g]): 
+			cntxt = cntxt + 1
+			flag = 1
+			babma2 = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[g])
+			this_tab['BornBy'] = searchterm
+			if babma2:
+				this_tab['BornIn'] = babma2.group()
+				this_tab['BabyMama'] = re.search(r'tabid.+',lines[start-1]).group().split()[1]
+				dest = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[j])
+				if dest:
+					this_tab['Destination'] = dest.group()
+	return this_tab,count,flag
+
+# looking by actions; could there be predestination for tabs 
+# (i.e. do you know this will be a searching tab, a news-reading tab, etc.)
+def showmethePages(tabIDs,lines):
+	actionlist = ['tab-pageshow','scroll','wheel','mouseup']
+	tab_newPages = {}
+	for tt in tabIDs:
+		tab_newPages[tt]= {actionlist[0]: 0, actionlist[1]: 0, actionlist[2]: 0} 
+	for j in range(len(lines)):
+		if re.search(r'"tabid":',lines[j]) and re.search(r'"action":',lines[j])
+			tabbie = re.search(r'"tabid":.+',lines[j])
+			if tabbie:
+				thisGuy = int(re.findall(r'\d+',tabbie.group().split()[1])[0])
+				action = re.search(r'"action":.+',lines[j]).group().split()[1]
+				for act in actionlist:
+ 					if re.findall('[a-z]',action) == re.findall('[a-z]',act):
+ 						tab_newPages[thisGuy][act] = tab_newPages[thisGuy][act] + 1
+
 def getBirths():
 	tabIDwPID = json.load(open("tab_ids_wPID.json"))
-	lines=open("random10.search.events").readlines()
+	tabID = json.load(open("Tabs_for_127raw.json"))
+	lines=open("raw-127.txt").readlines()
 	tbcnt = 0
 	mcnt = 0
-	cmd = 0
+	cntxt = 0
+	cmdclk = 0
+	tlbr = 0
 	movedTabs = []
 	tab_births = []
+	unknowns = []
 	for j in range(len(lines)):
 		tb = re.search(r'"tab-open"',lines[j])
 		if tb:
 			flag = 0
-			this_tab = []
+			this_tab = {}
 			tbcnt=tbcnt+1
-			for winnie in range(j+1,j+10):
-				winid = re.search('"windowid"',lines[winnie])
-				if winid:
-					this_tab.extend(['window ID:'])
-					this_tab.extend([int(re.findall(r'\d+',lines[winnie])[0])])
-					# break
-			for qp in range(j-1,j-10,-1):
-				tabid_srch = re.search(r'"tabid"',lines[qp])
-				if tabid_srch:
-					tabid = int(re.findall(r'\d+',lines[qp])[0])
-					# get participant number
-					for pid in tabIDwPID:
-						if pid[0] == tabid:
-							this_tab.extend(['PID:',pid[1]])
-							break
-					this_tab.extend(["this tabid:"])
-					this_tab.extend([tabid])
-					for jj in range(qp-1,qp-5,-1):
-						index = re.search(r'"index".+',lines[jj])
-						if index:
-							this_tab.extend([index.group()])
-							index = int(re.findall(r'\d+',lines[jj])[0])
-							# print "checking tab " + str(tabid) + " for movement in tabbar"
-							# movedTabs = findMoves(tabid,index,movedTabs,lines)
-							break
-			for l in range(j-1,j-40,-1):
-				act = re.search(r'"action": "mouseup"',lines[l])
-				tbshw = re.search(r'"tab-pageshow"',lines[l])
-				cmdsrch1 = re.search(r'command.+',lines[l])
-				if act:
-					this_tab.extend(['open action:',act.group()])
-					mcnt = mcnt + 1
-					for s in range(l,l-20,-1):
-						url = re.search(r'"url".+|"location".+|title.+',lines[s])
-						if url:
-							mama1 = re.search(re.escape(r'//')+"(.*?)"+re.escape('/'),lines[s])
-							maTabnum1 = re.search(r'"tabid.+"',lines[s])
-							if mama1 and not re.search(r'about',mama1.group()):
-								this_tab.extend(['MamaPage1:',mama1.group(1)])
+			# Get window ID
+			this_tab['thisWin']=int(re.findall(r'\d+',re.search(r'windowid.+',lines[j]).group().split()[1])[0])
+			# Get participant number
+			this_tab['PID']=int(lines[j].split()[0])
+			# Get tab ID number
+			this_tab['thisTab']=int(re.findall(r'\d+',re.search(r'tabid.+',lines[j]).group())[0])
+			# Get tab index in window
+			this_tab['Index']=int(re.findall(r'\d+',re.search(r'index.+',lines[j]).group().split()[1])[0])
+			# Get born on timestamp
+			this_tab['BornOn']=lines[j].split()[1]
+			# ...born by mouse click on self-opening link?
+			if re.search(r'"action": "mouseup"',lines[j-1]+lines[j-2]+lines[j-3]) and re.search(r'tab-deactivate',lines[j+1]+lines[j+2]) and re.search(r'tab-activate',lines[j+2]+lines[j+3]):
+				mcnt = mcnt + 1
+				this_tab['BornBy'] = 'left-mouse click'
+				flag = 1
+				for g in range(j-1,j-3,-1):
+					if re.search(r'"action": "mouseup"',lines[g]):
+						babma_ = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[g])
+						this_tab['BabyMama'] = re.search(r'tabid.+',lines[g]).group().split()[1]
+						if babma_:
+							this_tab['BornIn'] = babma_.group()
+						break
+				for gg in range(j+2,j+5):
+						if re.search(r'tab-ready',lines[g+2]):
+							dest = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[j])
+							if dest:
+								this_tab['Destination'] = dest.group()
 								break
-							elif url and re.search(r'about',url.group()):
-								this_tab.extend(['MamaPage1:',url.group()])
-								break
-							elif maTabnum1:
-								this_tab.extend(['MamaPageTabNum:',int(maTabnum1.group()[0])])
-				if cmdsrch1:
-					cmd = cmd + 1
-					this_tab.extend(['open cmd:',cmdsrch1.group()])
-					for mm in range(j+20,j-30,-1):
-						deact_srch = re.search(r'"tab-deactivate"',lines[mm])
-						if deact_srch:
-							for ds in range(mm,mm-10):
-								ma_srch1 = re.search(r'"url".+|"location".+',lines[ds])
-								if ma_srch1:
-									mama2 = re.search(re.escape(r'//')+"(.*?)"+re.escape('/'),lines[mm])
-									this_tab.extend(['MamaPage2:',mama2.group(1)])
-									break
-									masrchcnt = 3
-						ma_srch2 = re.search(r'"url".+|"location".+',lines[mm])
-						if ma_srch2 and not re.search('about',ma_srch2.group()):
-							mama3 = re.search(re.escape(r'//')+"(.*?)"+re.escape('/'),lines[mm])
-							this_tab.extend(['MamaPage3:',mama3.group(1)])
-							flag = 1
 							break
-				if tbshw: 
-					for g in range(l-1,l-20,-1):
-						cmdsrch2 = re.search(r'command.+|action.+',lines[g])
-						if cmdsrch2 and flag != 1:
-							cmd = cmd + 1
-							this_tab.extend([cmdsrch2.group()])
-						mama4 = re.search(re.escape(r'//')+"(.*?)"+re.escape('/'),lines[g])
-						maTabnum2 = re.search(r'"tabid.+"',lines[g])
-						if mama4 and not re.search(r'about',mama4.group()):
-							this_tab.extend(['MamaPage4:',mama4.group(1)])
-						elif maTabnum2:
-							this_tab.extend(['MamaPageTabNum',int(re.findall(r'\d+',maTabnum2.group())[0])])
-			j=len(lines)
-			tab_births.append(this_tab)
-	print "mouseups account for " + str(mcnt) + " of " + str(tbcnt) + " total tab opens"
-	print "commands account for " + str(cmd) + " of " + str(tbcnt) + " total tab opens"
-	mathz = int((cmd+mcnt)/float(tbcnt)*100)
+			# ...born by command + mouse click?
+			#for h in range(j-1,j-3):
+			if re.search(r'tab-pageshow',lines[j-1]+lines[j-2]+lines[j-3]) and not re.search(r'tab-deactivate',lines[j+1]+lines[j+2]): #re.search(r'"action": "mouseup"',lines[j-2]): 
+				cmdclk = cmdclk + 1
+				flag = 1
+				this_tab['BornBy'] = 'command + mouse click'
+				for h in range(j-1,j-3):
+					cmdsrch = re.search(r'tab-pageshow',lines[h])
+					if cmdsrch:
+						babma = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[h])
+						if babma:
+							this_tab['BornIn'] = babma.group()
+							this_tab['BabyMama'] = re.search(r'tabid.+',lines[j-1]).group().split()[1]
+					dest = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[j])
+					if dest:
+						this_tab['Destination']  = dest.group()
+			for g in range(j-1,j-5,-1):
+				if re.search(r'context-openlinkintab',lines[g]): 
+					cntxt = cntxt + 1
+					flag = 1
+					babma2 = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[g])
+					this_tab['BornBy'] = 'click-to-menu'
+					if babma2:
+						this_tab['BornIn'] = babma2.group()
+					this_tab['BabyMama'] = re.search(r'tabid.+',lines[j-1]).group().split()[1]
+				dest = re.search(re.escape('//')+"(.+)"+re.escape('/'),lines[j])
+				if dest:
+					this_tab['Destination'] = dest.group()
+			# ...by new tab button in chrome
+			for g in range(j-1,j-5,-1):
+				if re.search('xul:toolbarbutton',lines[g]):
+					tlbr = tlbr + 1
+					this_tab['BornBy'] =  'new tab button' 
+					this_tab['BabyMama'] = re.search(r'tabid.+',lines[j-1]).group().split()[1]
+					flag = 1
+			if flag != 1:
+				this_tab['BornBy'] = 'Immaculate Conception?'
+				unknowns.append(int(re.findall(r'\d+',re.search(r'tabid.+',lines[j]).group())[0]))
+			tab_births.append(this_tab)	
+	print "self-opening links account for " + str(mcnt) + " of " + str(tbcnt) + " total tab opens"
+	print str(round(mcnt/float(tbcnt)*100)) + " percent"
+	print "command + click accounts for " + str(cmdclk) + " of " + str(tbcnt) + " total tab opens"
+	print str(round(cmdclk/float(tbcnt)*100)) + " percent"
+	print "click-to-context menu accounts for " + str(cntxt) + " of " + str(tbcnt) + " total tab opens"
+	print str(round(cntxt/float(tbcnt)*100)) + " percent"
+	print "new tab button accounts for " + str(tlbr) + " of " + str(tbcnt) + " total tab opens"
+	print str(round(tlbr/float(tbcnt)*100)) + " percent"
+	mathz = int((cmdclk+mcnt+cntxt+tlbr)/float(tbcnt)*100)
 	print "accounting for " + str(mathz)+ " percent of all tab opens"
-	fh = open("tab_births.json","w")
+	fh = open("tab_births_raw127_dict.json","w")
 	fh.write(json.dumps(tab_births))
+	fh.close()
+	fh = open("UNKOWNtabs_raw127.json","w")
+	fh.write(json.dumps(unknowns))
 	fh.close()
 	return tab_births
 
@@ -124,45 +165,26 @@ def sortBirths():
 				user_tab_choice[thisSub][choiceNum] = user_tab_choice[thisSub][choiceNum] + 1
 		return user_tab_choice
 
-	tab_births = json.load(open("tab_births.json"))
+	tab_births = json.load(open("tab_births_raw127_dict.json"))
+	subbies = json.load(open("Raw127_Subs.json"))
 	newNvTb_cnt = 0
 	cntxt_cnt = 0
 	mseclk_cnt = 0
+	# user_tab_choice = {}
 	user_tab_choice = []
-	numPID = 10
-	for h in range(0,10):
-		user_tab_choice.append([0,0,0])
-	user_tab_choice.insert(0,['newNavigatorTab','context-openlinkintab','mouseup'])
+	numPID = len(subbies)
+	BornBys = ['left-mouse click','click-to-menu','command + mouse click','new tab button','Immaculate Conception?']
+	for h in range(numPID):
+		# user_tab_choice[h] = {'left-mouse click': 0,'click-to-menu': 0,'command + mouse click': 0,'Immaculate Conception?': 0}
+		user_tab_choice.append([0,0,0,0,0])
+	user_tab_choice.insert(0,BornBys)
 	for f in tab_births:
-		for i in f:
-			srch1=re.search(r'newNavigatorTab',str(i))
-			srch2=re.search(r'context-openlinkintab',str(i))
-			srch3=re.search(r'mouseup',str(i))
-			if srch1:
-				newNvTb_cnt = newNvTb_cnt + 1
-				user_tab_choice = accrueTabs(0,f)
-				# print tab_births.index(f)
-				# for ii in f:
-				# 	masrch = re.search('MamaPage',ii)
-				# 	if masrch and not re.search('blank',ii+1):
-				# 		print f[f.index(ii)+1]
-			if srch2:
-				cntxt_cnt = cntxt_cnt + 1
-				user_tab_choice = accrueTabs(1,f)
-			if srch3:
-				mseclk_cnt = mseclk_cnt + 1
-				user_tab_choice = accrueTabs(2,f)
-	print "New Navigator Tabs account for " + str(int(newNvTb_cnt/float(len(tab_births))*100)) + " percent of all tab births"
-	print "context-openlinkinnewtab's account for " + str(int(cntxt_cnt/float(len(tab_births))*100)) + " percentof all tab births"
-	print "Mouse clicks in prev page, auto-generating new tabs, account for " + str(int(mseclk_cnt/float(len(tab_births))*100)) + " percent of all tab births"
-	for aa in range(numPID):
-		SubDat = user_tab_choice[aa+1]
-		print "Subject " + str(aa) + " tab choice stats:"
-		print "total new tabs = " + str(sum(SubDat))
-		for g in range(len(SubDat)):
-			print "Percentage from " + str(user_tab_choice[0][g]) + ": " + str(int(SubDat[g]/float(sum(SubDat))*100))
-	fh = open("userTabChoices.json","w")
+		midwife = BornBys.index(f['BornBy'])
+		subNum = subbies.index(f['PID'])
+		user_tab_choice[subNum+1][midwife] = user_tab_choice[subNum+1][midwife] + 1
+	fh = open("userTabChoices_raw127.json","w")
 	fh.write(json.dumps(user_tab_choice))
 	fh.close()
 
-sortBirths()
+getBirths()
+# showmethePages(tabIDs,lines)
